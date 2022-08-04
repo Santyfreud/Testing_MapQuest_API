512fe680db1109d3a14ec58fd200c1f62501b1f1
SantyNavarro <santyfreud@gmail.com>
2022-08-04 17:54:56 -0500

    Implementing user entry functionality

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -6,18 +6,20 @@ import requests
 
 #Construcción de la URL de la API:
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
-origin = "Washington, D.C."
-destination = "Baltimore, Md"
 key = "gAQG3TGrxa2u2P4uR4mtkjg4oC59iOIJ"
-#Uso del método urlencode() para dar formato al valor de la URL:
-url = main_api + urllib.parse.urlencode({"key": key, "from": origin, "to": destination})
-#Mostrando al usuario la URL de la petición:
-print("URL: {}".format(url))
 
-#Realizando la petición GET a la API:
-#Se almacena el body de la respuesta en una variable y se le da formato json:
-json_data = requests.get(url).json()
-#Extrayendo el valor de la key "statuscode" para comprobar el estado de la petición:
-json_status = json_data["info"]["statuscode"]
-if json_status == 0:
-    print("API Status: {} = A successful route call.\n".format(json_status))
+#Implementando funcionalidad para entrada de usuario:
+while True:
+    origin = input("Starting location: ")
+    destination = input("Destination: ")
+    #Uso del método urlencode() para dar formato al valor de la URL:
+    url = main_api + urllib.parse.urlencode({"key": key, "from": origin, "to": destination})
+    #Mostrando al usuario la URL de la petición:
+    print("URL: {}".format(url))
+    #Realizando la petición GET a la API:
+    #Se almacena el body de la respuesta en una variable y se le da formato json:
+    json_data = requests.get(url).json()
+    #Extrayendo el valor de la key "statuscode" para comprobar el estado de la petición:
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print("API Status: {} = A successful route call.\n".format(json_status))
