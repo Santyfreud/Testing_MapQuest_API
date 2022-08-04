7c18090c9315ff821fe050877cb5269a68a51bb5
SantyNavarro <santyfreud@gmail.com>
2022-08-04 17:36:17 -0500

    Showing URL and Request Status Code to the user

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -6,14 +6,18 @@ import requests
 
 #Construcción de la URL de la API:
 main_api = "http://www.mapquestapi.com/directions/v2/route?"
-origin = "Rome, Italy"
-destination = "Frascati, Italy"
+origin = "Washington, D.C."
+destination = "Baltimore, Md"
 key = "gAQG3TGrxa2u2P4uR4mtkjg4oC59iOIJ"
 #Uso del método urlencode() para dar formato al valor de la URL:
 url = main_api + urllib.parse.urlencode({"key": key, "from": origin, "to": destination})
-#print(url)
+#Mostrando al usuario la URL de la petición:
+print("URL: {}".format(url))
 
 #Realizando la petición GET a la API:
 #Se almacena el body de la respuesta en una variable y se le da formato json:
 json_data = requests.get(url).json()
-print(json_data)
+#Extrayendo el valor de la key "statuscode" para comprobar el estado de la petición:
+json_status = json_data["info"]["statuscode"]
+if json_status == 0:
+    print("API Status: {} = A successful route call.\n".format(json_status))
